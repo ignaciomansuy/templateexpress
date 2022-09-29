469081ef08ab622f3650ffaca98b671b97fbdd1a
Ignacio Mansuy <ignacio.mansuy@uc.cl>
2022-09-29 12:21:21 -0300

    style: fixed

--- src/index.js
+++ src/index.js
@@ -3,4 +3,6 @@ import './config/db.js'
 import { PORT } from './config/config.js'
 
 app.listen(PORT)
+
+// eslint-disable-next-line no-console
 console.log('SERVER: Server started on port', app.get('port'))
